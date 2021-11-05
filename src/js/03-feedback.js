a80ee0519072a80a5abf1956b160b7264b91c899
Dimmac <84150134+Dimmac@users.noreply.github.com>
2021-11-05 02:19:51 +0200

    task_3_finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,9 @@
 import throttle from 'lodash.throttle';
-const refs = {
-  form: document.querySelector('.js-feedback-form'),
-  // input: document.querySelector('.js-feedback-form input'),
-  // textarea: document.querySelector('.js-feedback-form textarea'),
-};
 const FORM_STORAGE_KEY = 'feedback-form-state';
-// const EMAIL_STORAGE_KEY = 'email-key';
-// const MESSAGE_STORAGE_KEY = 'message-key';
 const THROTTLE_DELAY = 500;
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
 const formData = {};
 
 populateForm();
@@ -15,16 +11,9 @@ populateForm();
 refs.form.addEventListener('input', throttle(onInputFunction, THROTTLE_DELAY));
 refs.form.addEventListener('submit', onFormSubmit);
 
-refs.form.addEventListener('input', e => {
-  console.log(e.target.name);
-  console.log(e.target.value);
-  formData[e.target.name] = e.target.value;
-  console.log(formData);
-});
-
 function onFormSubmit(event) {
   event.preventDefault();
-  console.log('send form');
+  // console.log('send form');
   event.target.reset();
   localStorage.removeItem(FORM_STORAGE_KEY);
 }
@@ -34,13 +23,6 @@ function onInputFunction(event) {
   console.log(formData);
   localStorage.setItem(FORM_STORAGE_KEY, JSON.stringify(formData));
 }
-
-// function onTextareaInput(event) {
-//   //   localStorage.setItem('input.value');
-//   const message = event.target.value;
-//   localStorage.setItem('MESSAGE_STORAGE_KEY', message);
-//   // console.log(message);
-// }
 
 function populateForm() {
   let savedText = localStorage.getItem(FORM_STORAGE_KEY);
